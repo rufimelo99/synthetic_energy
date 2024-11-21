0cf3eaee0e7c2f395e737e322148aaa86115cbda
Rui Melo <rufimelo99@gmail.com>
2024-11-21 00:43:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License v3",
+        "License :: OSI Approved :: GPL License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
